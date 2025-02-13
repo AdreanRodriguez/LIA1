3a4f3368ad1324bb1722dea834d9571c9ed7a0aa
AdreanRodriguez <loco_bladare@hotmail.com>
2025-02-13 12:00:55 +0100

    cleaner code

--- src/hooks/useCleanup.ts
+++ src/hooks/useCleanup.ts
@@ -1,13 +1,11 @@
 import { useEffect } from "react";
 import { GameState } from "../utils/gameLogic";
-import { useGameLogic } from "./useGameLogic";
 
 let cleanupIntervalId: number | null = null;
 
 export function useCleanup(gameState: GameState) {
   useEffect(() => {
     if (gameState.isGameOver) {
-      useGameLogic;
       console.log("Game over, stopping cleanup interval.");
       if (cleanupIntervalId) {
         clearInterval(cleanupIntervalId);
